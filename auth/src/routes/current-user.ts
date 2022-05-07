f96ed19e28c3378c6cae33c545c92ad1d52a69d6
Giorgi Alvarez <giorgialvarez@gmail.com>
2022-05-07 19:57:18 -0400

    wrote logic to get current logged in user based on https stored cookie

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
 router.get("/api/users/currentuser", (req, res) => {
-  res.json({ success: true, route: "current user" });
+  if (!req.session?.jwt) {
+    return res.json({ user: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_SECRET!);
+    res.json(payload);
+  } catch (error) {
+    res.json({ user: null });
+  }
 });
 
 export { router as currentUserRouter };
