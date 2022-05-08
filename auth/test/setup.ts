62d45f609191e8b14e913f12117186bbc2946659
Giorgi Alvarez <giorgialvarez@gmail.com>
2022-05-07 21:52:28 -0400

    moved test folder, added a global signin test method

--- auth/test/setup.ts
+++ auth/test/setup.ts
@@ -1,9 +1,13 @@
 import { MongoMemoryServer } from "mongodb-memory-server";
 import mongoose from "mongoose";
-import { app } from "../app";
+import request from "supertest";
+import { app } from "../src/app";
+
+declare global {
+  var signin: () => Promise<string[]>;
+}
 
 let mongo: any;
-
 beforeAll(async () => {
   process.env.JWT_SECRET = "test";
   mongo = await MongoMemoryServer.create();
@@ -22,3 +26,17 @@ afterAll(async () => {
   await mongo.stop();
   await mongoose.connection.close();
 });
+
+global.signin = async () => {
+  const email = "test@test.com";
+  const password = "test";
+
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email, password })
+    .expect(201);
+
+  const cookie = response.get("Set-Cookie");
+
+  return cookie;
+};
