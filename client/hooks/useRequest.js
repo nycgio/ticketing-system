2343b060b053f70e5373ccacda2a6f5c00ff94c2
Giorgi Alvarez <giorgialvarez@gmail.com>
2022-05-08 00:42:38 -0400

    made a use request hook to handle request and errors

---
+++ client/hooks/useRequest.js
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
+import classes from "./hooks.module.scss";
+
+function useRequest({ url, method, body, onSuccess }) {
+  const [errors, setErrors] = useState([]);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (error) {
+      setErrors(
+        <div className={classes.error}>
+          <h4>Ooops...</h4>
+          <ul>
+            {error.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+}
+
+export default useRequest;
