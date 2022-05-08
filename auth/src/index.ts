76334cc2b211d0eca2da0e799ba189166bc42ec2
Giorgi Alvarez <giorgialvarez@gmail.com>
2022-05-07 20:43:01 -0400

    Test environment setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,38 +1,5 @@
-import express from "express";
-import "express-async-errors";
+import { app } from "./app";
 import mongoose from "mongoose";
-import cookieSession from "cookie-session";
-
-import { currentUserRouter } from "./routes/current-user";
-import { signinRouter } from "./routes/signin";
-import { signoutRouter } from "./routes/signout";
-import { signupRouter } from "./routes/signup";
-import { errorHandler } from "./middleware/error-handler";
-import { NotFoundError } from "./errors/not-found-error";
-
-const app = express();
-
-// allow proxying by nginx
-app.set("trust proxy", true);
-
-app.use(express.json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: true,
-  })
-);
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
-
-// error handlers
-app.all("*", async (req, res) => {
-  throw new NotFoundError();
-});
-app.use(errorHandler);
 
 const start = async () => {
   // check for env variables
