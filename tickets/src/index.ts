28bdf07b43e67ea0404ce8d58ca037a54fea1fc9
Giorgi Alvarez <giorgialvarez@gmail.com>
2022-05-16 18:20:50 -0400

    created a nats singleton wrapper and created events

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -1,5 +1,6 @@
 import { app } from "./app";
 import mongoose from "mongoose";
+import { natsWrapper } from "./nats-wrapper";
 
 const start = async () => {
   // check for env variables
@@ -12,6 +13,15 @@ const start = async () => {
   }
 
   try {
+    await natsWrapper.connect("ticketing", "lxslxls", "http://nats-svc:4222");
+
+    natsWrapper.client.on("close", () => {
+      console.log("NATS connection closed!");
+      process.exit();
+    });
+    process.on("SIGINT", () => natsWrapper.client.close());
+    process.on("SIGTERM", () => natsWrapper.client.close());
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("Connected to Tickets MongoDB");
   } catch (error) {
